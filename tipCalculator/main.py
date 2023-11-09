20ce99d847760f7846abd12102d377eca3ebbcba
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2023-11-09 12:20:39 +0530

    implemented tip calculator

---
+++ tipCalculator/main.py
@@ -0,0 +1,12 @@
+
+food_amount=float(input('Enter food amount $:'))
+tip_percentage=float(input('Enter tip tip percentage %: '))
+tip_amount=(food_amount)*(tip_percentage)/100
+total=int(food_amount)+tip_amount
+# print(total)
+#  lets add some amount sign like doller 
+# i need to convert the amount into string
+print ('Actual Amount $'+ str(food_amount))  
+#  here f used for formating
+print(f'Tip amount: ${tip_amount}')
+print('Total is $ ' +str(total))
